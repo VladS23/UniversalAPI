ae16843463ea409c2581b7bf6b1c09f62e6a8b8f
VladS23 <77384665+VladS23@users.noreply.github.com>
2023-04-23 12:39:06 +0300

    new project structure

---
+++ UniversalAPI/Http/Endpoints.cs
@@ -0,0 +1,109 @@
+﻿namespace UniversalAPI
+{
+    public class Endpoints
+    {
+        WebApplication app;
+        TokenManager tokens = new TokenManager();
+
+
+        public Endpoints(WebApplication app)
+        {
+            this.app = app;
+        }
+
+        public void  Run()
+        {
+            //tokens.AddToken("123456789");
+
+            app.Map("/vk/getOnlineStatus", HandleGetOnlineStatusRequest);
+
+            app.Map("/cb/USDpurchaseRate", HandleUSDpurchaseRateRequest);
+            app.Map("/cb/USDsellingRate", HandleUSDsellingRateRequest);
+
+            app.Run();
+        }
+
+        void HandleGetOnlineStatusRequest(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                if (ContainsTokenAndUserId(context.Request.Query))
+                {
+                    string token = context.Request.Query["token"];
+                    string userId = context.Request.Query["userId"];
+                    if (tokens.CheckToken(token))
+                    {
+                        var vk = new VK();
+                        await context.Response.WriteAsync(vk.GetOnlineStatus(userId));
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync("Invalid APIToken");
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync($"Invalid query");
+                }
+            });
+        }
+
+        void HandleUSDpurchaseRateRequest(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                if (ContainsToken(context.Request.Query))
+                {
+                    string token = context.Request.Query["token"];
+                    if (tokens.CheckToken(token))
+                    {
+                        var cb = new CentralBank();
+                        await context.Response.WriteAsync(cb.USDpurchaseRate());
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync("Invalid APIToken");
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync($"Invalid query");
+                }
+            });
+        }
+
+        void HandleUSDsellingRateRequest(IApplicationBuilder app)
+        {
+            app.Run(async (context) =>
+            {
+                if (ContainsToken(context.Request.Query))
+                {
+                    string token = context.Request.Query["token"];
+                    if (tokens.CheckToken(token))
+                    {
+                        var cb = new CentralBank();
+                        await context.Response.WriteAsync(cb.USDsellingRate());
+                    }
+                    else
+                    {
+                        await context.Response.WriteAsync("Invalid APIToken");
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync($"Invalid query");
+                }
+            });
+        }
+
+        bool ContainsTokenAndUserId(IQueryCollection query)
+        {
+            return query.ContainsKey("token") && query.ContainsKey("userId");
+        }
+
+        bool ContainsToken(IQueryCollection query)
+        {
+            return query.ContainsKey("token");
+        }
+    }
+}
--- UniversalAPI/Program.cs
+++ UniversalAPI/Program.cs
@@ -4,44 +4,6 @@ var builder = WebApplication.CreateBuilder();
 
 var app = builder.Build();
 
-TokenManager tokens = new TokenManager();
-//tokens.AddToken("123456789");
+var endPoints = new Endpoints(app);
+endPoints.Run();
 
-app.Map("/{token}/vk/getOnlineStatus/{userId}", HandleGetOnlineStatusRequest);
-
-app.Map("/{token}/cb/USDpurchaseRate", HandleUSDpurchaseRateRequest);
-app.Map("/{token}/cb/USDsellingRate", HandleUSDsellingRateRequest);
-
-app.Run();
-
-
-
-string HandleGetOnlineStatusRequest(string token, string userId)
-{
-    if (tokens.CheckToken(token))
-    {
-        var vk = new VK();
-        return vk.GetOnlineStatus(userId);
-    }
-    return "Invalid APIToken";
-}
-
-string HandleUSDpurchaseRateRequest(string token)
-{
-    if (tokens.CheckToken(token))
-    {
-        var cb = new CentralBank();
-        return cb.USDpurchaseRate();
-    }
-    return "Invalid APIToken";
-}
-
-string HandleUSDsellingRateRequest(string token)
-{
-    if (tokens.CheckToken(token))
-    {
-        var cb = new CentralBank();
-        return cb.USDsellingRate();
-    }
-    return "Invalid APIToken";
-}
